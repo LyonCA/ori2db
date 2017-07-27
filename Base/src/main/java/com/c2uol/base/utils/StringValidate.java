6bf27259baf2e5b5ec6c82a049b9cd2a88cfcc07
lyon <lyon4j@yahoo.com>
2017-07-28 00:11:03 +0800

    redis 多数据源数据池完成

--- Base/src/main/java/com/c2uol/base/utils/StringValidate.java
+++ Base/src/main/java/com/c2uol/base/utils/StringValidate.java
@@ -14,6 +14,16 @@ public class StringValidate {
 
 	}
 
+	/**
+	 * 
+	 * @描述: 字符为空
+	 * @参数: @param str
+	 * @参数: @return
+	 * @返回值: boolean
+	 * @版本: v1.0
+	 * @时间: 2017年7月27日下午10:25:11
+	 *
+	 */
 	public static boolean isEmpty(String str) {
 		if (str == null || str.trim().length() == 0) {
 			return true;
@@ -21,6 +31,16 @@ public class StringValidate {
 		return false;
 	}
 
+	/**
+	 * 
+	 * @描述: 字符为数字
+	 * @参数: @param str
+	 * @参数: @return
+	 * @返回值: boolean
+	 * @版本: v1.0
+	 * @时间: 2017年7月27日下午10:25:17
+	 *
+	 */
 	public static boolean isInteger(String str) {
 		if (isEmpty(str)) {
 			return false;
